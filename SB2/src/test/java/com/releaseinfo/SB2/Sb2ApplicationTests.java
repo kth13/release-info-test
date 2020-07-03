3ce81696b2a16b27f2ab57ed89423d2d0bc379a7
kumarthangavel <kumar.thangavel@sita.aero>
2020-07-03 16:55:06 +0100

    Initial version of SB2

---
+++ SB2/src/test/java/com/releaseinfo/SB2/Sb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.releaseinfo.SB2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
