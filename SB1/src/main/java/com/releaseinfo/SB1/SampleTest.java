3e5aa09b82919eeb196eeb765f17177171fb04e8
kumarthangavel <kumar.thangavel@sita.aero>
2020-07-03 15:33:56 +0100

    the release info logs

--- SB1/src/main/java/com/releaseinfo/SB1/SampleTest.java
+++ SB1/src/main/java/com/releaseinfo/SB1/SampleTest.java
@@ -4,5 +4,7 @@ public class SampleTest {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("This is testing the release info");
     }
 }
