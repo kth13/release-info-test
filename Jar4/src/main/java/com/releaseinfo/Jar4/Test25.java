e0a17b6f417b080f006392ab80d6ccb7895518fd
kumarthangavel <kumar.thangavel@sita.aero>
2020-07-05 00:28:25 +0100

    RLI-21|Initial commit

---
+++ Jar4/src/main/java/com/releaseinfo/Jar4/Test25.java
@@ -0,0 +1,10 @@
+package com.releaseinfo.Jar4;
+
+public class Test25 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println("This is testing the release info");
+    }
+}
