c83ea81640b2518a0f8150068869f481605fa317
kumarthangavel <kumar.thangavel@sita.aero>
2020-07-04 23:53:00 +0100

    RLI-11|kthangavel|committing Jar2

---
+++ Jar2/src/main/java/com/releaseinfo/Jar2/Test20.java
@@ -0,0 +1,10 @@
+package com.releaseinfo.Jar2;
+
+public class Test20 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println("This is testing the release info");
+    }
+}
---
+++ Jar2/src/test/java/com/releaseinfo/Jar2/Sb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.releaseinfo.Jar2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
